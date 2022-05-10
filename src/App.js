a37179fe11957ccbbb11beadf822192bf8a6c4cc
Pablo <pablotachella95@gmail.com>
2022-05-10 20:01:33 -0300

    Title: Implementation of public and private routes
    Description: It is validated if the user is logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { Login } from './components/views/Login/Login';
 import { Register } from './components/views/Register/Register';
@@ -6,10 +6,23 @@ import { Tasks } from './components/views/Tasks/Tasks';
 import { Error404 } from './components/views/Error404/Error404';
 import './App.css';
 
+const RequireAuth = ({ children }) => {
+  if (!localStorage.getItem("logged")) {
+    return <Navigate to="/login" replace={true} />
+  }
+  return children
+}
+
 export const App = () =>
   <Routes>
+    <Route path="/"
+      element={
+        <RequireAuth>
+          <Tasks />
+        </RequireAuth>
+      } 
+    />
     <Route path="/register" element={<Register />} />
     <Route path="/login" element={<Login />} />
-    <Route path="/" element={<Tasks />} />
     <Route path="*" element={<Error404 />} />
   </Routes>
