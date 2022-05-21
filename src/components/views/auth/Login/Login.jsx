59bb31bebafedb46f77539e9df9b4bec6fbc2fc0
Pablo <pablotachella95@gmail.com>
2022-05-20 23:53:36 -0300

    Title: Added logic for login
    Description: Added fields validation in Login component
     User token saved in localStorage

--- src/components/views/auth/Login/Login.jsx
+++ src/components/views/auth/Login/Login.jsx
@@ -1,51 +1,69 @@
 import { useFormik } from "formik";
 import { useNavigate, Link } from "react-router-dom";
+import * as Yup from "yup";
 
 import "../Auth.styles.css";
+
+const { REACT_APP_API_ENDPOINT: API_ENDPOINT } = process.env
 
 export const Login = () => {
   const navigate = useNavigate();
 
   const initialValues = {
-    email: '',
+    userName: '',
     password: ''
   }
 
-  const validate = values => {
-    const errors = {}
+  const required = "* Campo obligatorio"
 
-    if (!values.email) {
-      errors.email = 'El email es requerido.'
-    }
-    if (!values.password) {
-      errors.password = 'La contraseña es requerida.'
-    }
-
-    return errors
-  }
+  const validationSchema = () =>
+    Yup.object().shape({
+      userName: Yup.string()
+        .min(4, "La cantidad mínima de caracteres es 4")
+        .required(required),
+      password: Yup.string().required(required),
+    })
 
   const onSubmit = () => {
-    localStorage.setItem('logged', 'yes')
-    navigate("/", { replace: true })
+    const { userName, password } = values
+    fetch(`${API_ENDPOINT}auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userName,
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status_code === 200) {
+          localStorage.setItem("token", data?.result?.token)
+          navigate("/", { replace: true })
+        }
+      })
   }
 
-  const formik = useFormik({ initialValues, validate, onSubmit })
+  const formik = useFormik({ initialValues, validationSchema, onSubmit })
 
-  const { handleChange, handleSubmit, errors, values } = formik
+  const { handleChange, handleSubmit, errors, touched, handleBlur, values } = formik
 
   return (
     <div className='auth'>
       <form onSubmit={handleSubmit}>
         <h1>Iniciar sesión</h1>
         <div>
-          <label>Email</label>
+          <label>Nombre de usuario</label>
           <input
-            type='email'
-            name='email'
-            value={values.email}
+            type='text'
+            name='userName'
+            value={values.userName}
             onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.userName && touched.userName ? "error" : ""}
           />
-          {errors.email && <span>{errors.email}</span>}
+          {errors.userName && touched.userName && <span>{errors.userName}</span>}
         </div>
         <div>
           <label>Contraseña</label>
@@ -54,8 +72,10 @@ export const Login = () => {
             name='password'
             value={values.password}
             onChange={handleChange}
+            onBlur={handleBlur}
+            className={errors.password && touched.password ? "error" : ""}
           />
-          {errors.password && <span>{errors.password}</span>}
+          {errors.password && touched.password &&  <span>{errors.password}</span>}
         </div>
         <div>
           <button type='submit'>Enviar</button>
