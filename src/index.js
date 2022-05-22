fceffe6d4c1b42e527d4980542d298a54b3ff558
Pablo <pablotachella95@gmail.com>
2022-05-22 12:20:56 -0300

    Title: Redux Implementation
    Description: Installed Redux and created the first reducer
     The application was evolved with Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
 
 import { App } from './App';
+import { store } from "./store/store";
 import './index.css';
 
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
