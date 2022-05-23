84bcace175b235fe56e617d8d2caad5d4ddb2337
Pablo <pablotachella95@gmail.com>
2022-05-23 08:49:37 -0300

    Title: Added number of tasks created in the Header component
    Description: Consuming the same state in different components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import "./Header.styles.css";
 
 export const Header = () => {
   const navigate = useNavigate()
+
+  const { tasks } = useSelector((state) => {
+    return state.tasksReducer
+  })
 
   const handleLogout = () => {
     localStorage.removeItem("token")
@@ -15,6 +20,7 @@ export const Header = () => {
     <header>
       <img src="/img/logo.png" alt="Logo" />
       <div className="wrapper_right_header">
+        <div className="black">Tareas creadas: {tasks?.length}</div>
         <div className="black">{localStorage.getItem("userName")}</div>
         <div onClick={handleLogout}>x</div>
       </div>
